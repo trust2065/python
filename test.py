7937426c2bfba42ba07d6e0865998652e217b261
Choco Li <cli@modernstar.com>
2018-10-12 14:15:31 +1100

    able to get feed list

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
 import facebook
-graph = facebook.GraphAPI(access_token='EAAemR6Q27nsBAJA8Q57ZBgGc2cENCLjNG1K1pZABfmiGZBkpHmn6tBPUXYUezZBT5oSoYyDtQteSeBWB6UzjmTn5e46k1FhCwZASTnzDQYaKx5ejrEe1eyzXi8eIv0t5AZCoaaj7gzT7lt3MOZBuXVYcnDjbkh4wnUjmn4LLKAzRe1OV1Iwvn7LgjCTUJIko5iOtZAqcM21XEAZDZD', version='2.7') 
+graph = facebook.GraphAPI(access_token='EAAemR6Q27nsBAJ6cyZADCXvuJxpZBt4zHUwcn5zIh7jhzj07m40ZC8h3rbqQlHlBzlpAxS2WVCg9qsTo2Wsj6xKymTW7ZBZBn971hxNTBhj77FEcBfqsGrpkfnJtlSfZBb1r3MQJZB88b1nvUkEX1JX99Qp0B57FZC1DnftgYcfoJIZC5Bo7fJ1YWnh18ZAcBhwLoZD', version='2.7') 
  
 # Get the messsage of a post
-post = graph.get_object(id='1902194509828194_1898238116890500')
- 
-print(post['message']) 
+me = graph.get_object(id='me')
+feedList = graph.get_connections(id='me', connection_name='feed')
+
+# print(feedList)
+
+for feed in feedList['data']:
+    print(feed['id'])
+    print('')
+    print(feed['message'])
+
+# like your post(doesn't work)
+# postInfo = feedList['data'][0]
+# print(postInfo)
+# postId = postInfo['id']
+# print(postId)
+
+# graph.put_like(postId)
+
+
 # Get the created_time of the posts
- 
 # post_ids = ['665691423504646_1222774031129713', '665691423504646_1209392062467910']
 # posts = graph.get_objects(ids=post_ids) 
  
